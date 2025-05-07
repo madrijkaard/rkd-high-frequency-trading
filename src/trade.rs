39ce9f5d0d29cb8c3def54d0c260ab8af5656682
madrijkaard <mad.rijkaard@gmail.com>
2025-05-07 20:20:19 -0300

    Initial commit

--- src/trade.rs
+++ src/trade.rs
@@ -23,29 +23,29 @@ pub fn generate_trade(candlesticks: Vec<Candlestick>) -> Trade {
     let log_min = min_low.ln();
     let log_max = max_high.ln();
     let log_zone_4 = (log_min + log_max) / 2.0;
-    let log_mid_min = (log_min + log_zone_4) / 2.0;
-    let log_mid_max = (log_max + log_zone_4) / 2.0;
-    let log_mid_min_inner = (log_mid_min + log_zone_4) / 2.0;
-    let log_mid_max_inner = (log_mid_max + log_zone_4) / 2.0;
-    let log_zone_1 = (log_min + log_mid_min) / 2.0;
-    let log_above_max = (log_max + log_mid_max) / 2.0;
+    let log_zone_2 = (log_min + log_zone_4) / 2.0;
+    let log_zone_6 = (log_max + log_zone_4) / 2.0;
+    let log_zone_3 = (log_zone_2 + log_zone_4) / 2.0;
+    let log_zone_5 = (log_zone_6 + log_zone_4) / 2.0;
+    let log_zone_1 = (log_min + log_zone_2) / 2.0;
+    let log_zone_7 = (log_max + log_zone_6) / 2.0;
 
+    let price_zone_7 = log_zone_7.exp();
+    let price_zone_6 = log_zone_6.exp();
+    let price_zone_5 = log_zone_5.exp();
     let price_zone_4 = log_zone_4.exp();
-    let price_mid_min = log_mid_min.exp();
-    let price_mid_max = log_mid_max.exp();
-    let price_mid_min_inner = log_mid_min_inner.exp();
-    let price_mid_max_inner = log_mid_max_inner.exp();
+    let price_zone_3 = log_zone_3.exp();
+    let price_zone_2 = log_zone_2.exp();
     let price_zone_1 = log_zone_1.exp();
-    let price_above_max = log_above_max.exp();
 
     Trade {
         zone_max: format!("{:.8}", max_high),
-        zone_7: format!("{:.8}", price_above_max),
-        zone_6: format!("{:.8}", price_mid_max),
-        zone_5: format!("{:.8}", price_mid_max_inner),
+        zone_7: format!("{:.8}", price_zone_7),
+        zone_6: format!("{:.8}", price_zone_6),
+        zone_5: format!("{:.8}", price_zone_5),
         zone_4: format!("{:.8}", price_zone_4),
-        zone_3: format!("{:.8}", price_mid_min_inner),
-        zone_2: format!("{:.8}", price_mid_min),
+        zone_3: format!("{:.8}", price_zone_3),
+        zone_2: format!("{:.8}", price_zone_2),
         zone_1: format!("{:.8}", price_zone_1),
         zone_min: format!("{:.8}", min_low),
         current_price,
